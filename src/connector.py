42375461165f570d73263da64194c7aad3cc400b
p32929 <p32929@yahoo.com>
2022-07-15 08:44:23 +0600

    updated connector

--- src/connector.py
+++ src/connector.py
@@ -1,5 +1,3 @@
-from src.modules.user.user_controller import user_controller
-from src.modules.root.root_controller import root_controller
 from datetime import timedelta
 from flask import Flask
 from flask_jwt_extended import JWTManager
@@ -28,5 +26,7 @@ swagger.register(app)
 redoc = OpenAPI(endpoint="/docs/redoc/", ui='redoc', name="redoc")
 redoc.register(app)
 # BP REG
+from src.modules.root.root_controller import root_controller
 app.register_blueprint(root_controller, url_prefix='/')
+from src.modules.user.user_controller import user_controller
 app.register_blueprint(user_controller, url_prefix='/users')
